2b53cc2efa1bc0a56664b106a11e040f2481923e
asadbek-ip <dasturchi6678@gmail.com>
2023-07-22 11:58:03 +0500

    Initial Commit

---
+++ udacitylearning/udacitylearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  udacitylearningApp.swift
+//  udacitylearning
+//
+//  Created by Asadbek on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct udacitylearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
